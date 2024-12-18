c8f149471e829787bf8920e71ea584c764d363e4
Rhys Tumelty <rhys@tumelty.co.uk>
2024-12-18 19:47:54 +0000

    Fixed actions

--- build-scripts/build-iossimulator.sh
+++ build-scripts/build-iossimulator.sh
@@ -6,7 +6,7 @@ echo "Step 1: Configure"
 
 mkdir -p build/macos-iossimulator
 cd build/macos-iossimulator
-cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=ios.toolchain.cmake -DPLATFORM=SIMULATORARM64 -DUSINGIOS=YES
+cmake ../.. -G Xcode -DCMAKE_TOOLCHAIN_FILE=ios.toolchain.cmake -DPLATFORM=SIMULATORARM64 -DUSINGIOS=YES
 
 echo "Step 2: Build"
 cmake --build build --config Release
